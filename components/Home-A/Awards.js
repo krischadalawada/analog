b4139d1155dbe72ad893938fa6f37773faeb125c
Sai Krishna <krischadalawada@gmail.com>
2023-03-29 08:48:09 +0530

    Awards fix

--- components/Home-A/Awards.js
+++ components/Home-A/Awards.js
@@ -10,7 +10,7 @@ const Awards = () => {
          <section className="slider__area include-bg d-flex align-items-center bg-awards" style={{ caretColor: 'transparent' }}>
             <div className="row align-items-center">
                <div className="col-xxl-6 col-lg-6">
-                  <div className="slider__content-2" style={{ textAlign: 'center', padding: '10%' }}>
+                  <div className="slider__content-2" style={{ textAlign: 'left', padding: '10%' }}>
                      <h3 className="slider__title-2">India's Leading Brand Award, 2023 (Rising Star)</h3>
                      <h3 className="slider__title-3"><b>Asian Brand & Leadershp Conclave</b>, Dubai</h3>
                      <h3 className="slider__title-sub">For excellence in training sector & helping to enrich the education sector in India.</h3>
