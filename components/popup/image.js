013a2e262b3c252efeff5e5d3e1cdc91541c19da
Sai Krishna <krischadalawada@gmail.com>
2023-05-07 19:33:32 +0530

    Popup changes

--- components/popup/image.js
+++ components/popup/image.js
@@ -13,7 +13,10 @@ const ImagePopup = () => {
       isOpen={modalIsOpen}
       onRequestClose={closeModal}
     >
-      <a href='https://astra.analogeducation.in/ofl-civils-prep-with-astra?utm_source=Analog_Website&utm_medium=Online&utm_campaign=Astra'><img className="popup-image" src="assets/images/popup/popupimg.jpg" alt="Your Image" /></a>
+      <a href='https://astra.analogeducation.in/ofl-civils-prep-with-astra?utm_source=Analog_Website&utm_medium=Online&utm_campaign=Astra'>
+        <img className="popup-image" src="assets/images/popup/popupimg.jpg" alt="Astra Popup Image" />
+      </a>
+      <p onClick={() => closeModal()} className='modal_close pointer'>Hide</p>
     </Modal>
   );
 };
