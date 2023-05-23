af18624fcf11591e966b695d5d5d4b7f1ab0d77a
Nikhil Vema <124234082+NikhilMNM@users.noreply.github.com>
2023-05-23 18:42:57 +0530

    astra changes

--- components/Home-A/Events.js
+++ components/Home-A/Events.js
@@ -40,7 +40,7 @@ const EventsHome = () => {
                line: 'Scholarship Test'
             },
             {
-               line: 'Next on 10.06.2023'
+               line: 'Next on 18.06.2023'
             }
          ]
       },
