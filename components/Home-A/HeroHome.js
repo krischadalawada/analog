bca0944ef02bcea1ff545c0083ce345767da3770
Nikhil Vema <124234082+NikhilMNM@users.noreply.github.com>
2023-05-27 12:11:40 +0530

    banner in mainsite

--- components/Home-A/HeroHome.js
+++ components/Home-A/HeroHome.js
@@ -1,17 +1,46 @@
 import Link from 'next/link';
 import useWindowSize from '../Common-A/Window';
 
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 const HeroHome = () => {
 
    const { width, height } = useWindowSize()
 
+   const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+    };
+
    return (
       <>
-         <section className="slider__area slider__height-2 include-bg d-flex align-items-center bg-home" style={{ caretColor: 'transparent' }}>
-            <div className="container">
-               <div className="row align-items-center">
-                  <div className="col-xxl-6 col-lg-6">
-                     <div className="slider__content-2 mt-30">
+         <section className="bg-home" style={{ caretColor: 'transparent' }}>
+            <div className="align-items-center" style={{paddingTop:'23vh'}}>
+            <div className="banner-slider " >
+                     <Slider {...settings}>
+                     <div>
+                        <img style={{ width: '100%'}} src="assets/images/home/Slide_1.jpg" alt="Banner 1" />
+                        
+                     </div>
+                     <div>
+                        <img style={{ width: '100%'}} src="assets/images/home/Slide_2.jpg" alt="Banner 2" />
+                        
+                     </div>
+                     <div>
+                        <img style={{ width: '100%'}} src="assets/images/home/Slide_3.jpg" alt="Banner 3" />
+                        
+                     </div>
+                     </Slider>
+                  </div>
+            </div>
+            <div className="slider__content-2 mt-30 pb-30 container">
                         <h3 className="slider__title-2">Be UPSC Exam-Ready<br /> in less than a Year.</h3>
                         <h3 className="slider__title-sub">New Batches for IAS 2024 <br />starting from 15th April 2023</h3>
                         <Link href="/contact-us">
@@ -22,17 +51,7 @@ const HeroHome = () => {
                            <a className="tp-btn-primary" style={{ marginTop: width < 400 ? '3%' : 0 }}>New Batches <i className="fa-regular fa-arrow-right fa-ri">
                            </i></a>
                         </Link>
-                     </div>
-                  </div>
-                  <div className="col-xxl-6 col-lg-6">
-                     <div className="slider__thumb-2 p-relative">
-                        <span className="slider__thumb-mask">
-                           <img style={{ width: '100%'}} src="assets/images/home/HomeBannerPic_1@2x.png" alt="" />
-                        </span>
-                     </div>
-                  </div>
                </div>
-            </div>
          </section>
       </>
    );
