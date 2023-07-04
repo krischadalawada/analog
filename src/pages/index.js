1426c4fa7a773f6db9844565eb99dd6b04743fb1
Nikhil Vema <124234082+NikhilMNM@users.noreply.github.com>
2023-07-04 18:07:55 +0530

    remove popup

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ import EventsHome from '../../components/Home-A/Events';
 import Testimonials from '../../components/Home-A/Testimonials';
 import ContactUs from '../../components/Contact/ContactUs';
 import Awards from '../../components/Home-A/Awards';
-import ImagePopup from '../../components/popup/image';
 
 export default function Home() {
   return (
@@ -23,7 +22,7 @@ export default function Home() {
       
       <Header />
       <div style={{ backgroundColor: '#E4F2F8' }}>
-      <ImagePopup/>
+   
         <HeroHome />
         <Awards />
         <Exams />
